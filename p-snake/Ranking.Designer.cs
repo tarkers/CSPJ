4b50a633a500f624dc0db646d18f87553385c188
vianshang <stu121316@yahoo.com.tw>
2022-01-03 15:27:09 +0800

    Add Ranking Form Interface

--- p-snake/Ranking.Designer.cs
+++ p-snake/Ranking.Designer.cs
@@ -31,10 +31,27 @@ namespace p_snake
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.txtSnake = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtBlocks = new System.Windows.Forms.TextBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.txtCar = new System.Windows.Forms.TextBox();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.txtBalloon = new System.Windows.Forms.TextBox();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.txtEgg = new System.Windows.Forms.TextBox();
             this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.txtRank = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            this.tabPage7.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -42,50 +59,153 @@ namespace p_snake
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
             this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.tabControl1.Location = new System.Drawing.Point(2, 0);
+            this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(800, 452);
+            this.tabControl1.Size = new System.Drawing.Size(65535, 65535);
             this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tabPage1.Controls.Add(this.txtSnake);
+            this.tabPage1.Location = new System.Drawing.Point(4, 40);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage1.Size = new System.Drawing.Size(792, 420);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(65527, 65491);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "貪食蛇";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // txtSnake
+            // 
+            this.txtSnake.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtSnake.Location = new System.Drawing.Point(57, 29);
+            this.txtSnake.Multiline = true;
+            this.txtSnake.Name = "txtSnake";
+            this.txtSnake.Size = new System.Drawing.Size(666, 347);
+            this.txtSnake.TabIndex = 0;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 28);
+            this.tabPage2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tabPage2.Controls.Add(this.txtBlocks);
+            this.tabPage2.Location = new System.Drawing.Point(4, 40);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 420);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(65527, 65491);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "俄羅斯方塊";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtBlocks
+            // 
+            this.txtBlocks.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtBlocks.Location = new System.Drawing.Point(56, 28);
+            this.txtBlocks.Multiline = true;
+            this.txtBlocks.Name = "txtBlocks";
+            this.txtBlocks.Size = new System.Drawing.Size(687, 344);
+            this.txtBlocks.TabIndex = 0;
             // 
             // tabPage3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 28);
+            this.tabPage3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tabPage3.Controls.Add(this.txtGuess);
+            this.tabPage3.Location = new System.Drawing.Point(4, 40);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(792, 420);
+            this.tabPage3.Size = new System.Drawing.Size(65527, 65491);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "戰車遊戲";
-            this.tabPage3.UseVisualStyleBackColor = true;
+            this.tabPage3.Text = "1A2B猜數字";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtGuess.Location = new System.Drawing.Point(56, 30);
+            this.txtGuess.Multiline = true;
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(696, 346);
+            this.txtGuess.TabIndex = 0;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tabPage5.Controls.Add(this.txtCar);
+            this.tabPage5.Location = new System.Drawing.Point(4, 40);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(65527, 65491);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "賽車遊戲";
+            // 
+            // txtCar
+            // 
+            this.txtCar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtCar.Location = new System.Drawing.Point(38, 34);
+            this.txtCar.Multiline = true;
+            this.txtCar.Name = "txtCar";
+            this.txtCar.Size = new System.Drawing.Size(690, 332);
+            this.txtCar.TabIndex = 0;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tabPage6.Controls.Add(this.txtBalloon);
+            this.tabPage6.Location = new System.Drawing.Point(4, 40);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(65527, 65491);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "射氣球";
+            // 
+            // txtBalloon
+            // 
+            this.txtBalloon.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtBalloon.Location = new System.Drawing.Point(49, 30);
+            this.txtBalloon.Multiline = true;
+            this.txtBalloon.Name = "txtBalloon";
+            this.txtBalloon.Size = new System.Drawing.Size(692, 341);
+            this.txtBalloon.TabIndex = 0;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tabPage7.Controls.Add(this.txtEgg);
+            this.tabPage7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.tabPage7.Location = new System.Drawing.Point(4, 40);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(65527, 65491);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "接蛋遊戲";
+            // 
+            // txtEgg
+            // 
+            this.txtEgg.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtEgg.Location = new System.Drawing.Point(61, 26);
+            this.txtEgg.Multiline = true;
+            this.txtEgg.Name = "txtEgg";
+            this.txtEgg.Size = new System.Drawing.Size(654, 346);
+            this.txtEgg.TabIndex = 0;
             // 
             // tabPage4
             // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 28);
+            this.tabPage4.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.tabPage4.Controls.Add(this.txtRank);
+            this.tabPage4.Location = new System.Drawing.Point(4, 40);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(792, 420);
+            this.tabPage4.Size = new System.Drawing.Size(65527, 65491);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "總排名";
-            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // txtRank
+            // 
+            this.txtRank.Location = new System.Drawing.Point(61, 38);
+            this.txtRank.Multiline = true;
+            this.txtRank.Name = "txtRank";
+            this.txtRank.Size = new System.Drawing.Size(661, 325);
+            this.txtRank.TabIndex = 0;
             // 
             // Ranking
             // 
@@ -97,7 +217,22 @@ namespace p_snake
             this.Text = "排行榜";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Ranking_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Ranking_FormClosed);
+            this.Load += new System.EventHandler(this.Ranking_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage6.PerformLayout();
+            this.tabPage7.ResumeLayout(false);
+            this.tabPage7.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -109,5 +244,15 @@ namespace p_snake
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.TextBox txtSnake;
+        private System.Windows.Forms.TextBox txtBlocks;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.TextBox txtCar;
+        private System.Windows.Forms.TextBox txtBalloon;
+        private System.Windows.Forms.TextBox txtEgg;
+        private System.Windows.Forms.TextBox txtRank;
     }
 }
